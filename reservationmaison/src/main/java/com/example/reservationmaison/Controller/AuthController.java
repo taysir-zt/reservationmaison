143874d1e317157193fe86ad29e5fa6926a00065
Tayssir Zayet <taysirzayet@outlook.com>
2024-11-08 13:24:05 -0600

    Fix line endings

--- reservationmaison/src/main/java/com/example/reservationmaison/Controller/AuthController.java
+++ reservationmaison/src/main/java/com/example/reservationmaison/Controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.example.reservationmaison.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -9,4 +9,22 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RequestMapping("api/v1/Client")
 
 public class AuthController {
+    @Autowired
+    private ClientService clientservice;
+
+    @PostMapping(path = "/register")
+    public String saveClient(@RequestBody ClientDTO clientDTO){
+
+        String id = clientservice.addClient(clientDTO);
+        return id;
+    }
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<?> loginClient(@ResponseBody LoginDTO loginDTO){
+
+        LoginMessage loginMessage = clientservice.loginMessage(loginDTO);
+        return ResponseEntity.ok(loginMessage);
+    }
+
+
 }
